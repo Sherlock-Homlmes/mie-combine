9f68e655b1bbe384a6091739b076f840f0006345
Sherlock-Homlmes <dbsiksfikf@gmail.com>
2022-03-08 19:24:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -287,6 +287,37 @@ async def on_ready():
     print(client.user.id)
     print(' ')
 
+@client.event
+async def on_member_join(member):
+
+  embed= discord.Embed(
+  title = "**Chào mừng "+member.name+" đến với Cộng Đồng học tập BetterMe**",
+  description =" Hãy giới thiệu bản thân cho mọi người biết ở <#894594032947310602> nhé",
+  colour = discord.Colour.gold()
+  )
+
+  welcome_channel = get(member.guild.channels, id=894594032947310602)
+
+  pfp = member.avatar_url
+  field1="Hãy đọc kĩ luật ở <#880369537449619476> nha"
+  field2="Nếu bạn chưa biết cách dùng Discord thì ở <#915949063403347968> có đó"
+  field3="Hãy vào <#891909866355048548> để nhận role tương ứng nha"
+
+  embed.set_footer(text='''Chúc bạn có 1 khoảng thời gian vui vẻ. 
+BetterMe-Better everyday''')
+  embed.set_image(url="https://i.ibb.co/Wvw2CY9/Betterme.png")
+  embed.set_thumbnail(url=pfp)
+  embed.add_field(name="**Đọc kĩ luật**",value=field1,inline=False)
+  embed.add_field(name="**Hướng dẫn cơ bản**",value=field2,inline=False)
+  embed.add_field(name="**Set role**",value=field3,inline=False)
+	
+  msg = await welcome_channel.send(content=member.mention,embed=embed)
+  await asyncio.sleep(660)
+  await msg.delete()
+
+
+
+
 can_clear = True
 @client.event
 async def on_voice_state_update(member, member_before, member_after):

6c67af9b1af6773c6870bbb31e18c55f4e2e495a
Beowulf Cluster <dbsiksfikf@gmail.com>
2022-09-20 21:06:06 +0700

    fix confession

--- confession/confession.py
+++ confession/confession.py
@@ -1,5 +1,3 @@
-import asyncio
-
 from base import (
     # necess
     discord,
@@ -16,6 +14,9 @@ from base import (
     confession_dropdown_id,
     confession_category_id
 )
+
+import asyncio
+from typing import Optional, Union
 
 from feature_func.mongodb import open_database, write_database
 database_directory = "confession"
@@ -54,184 +55,178 @@ async def kick(ctx: context.Context):
     await ctx.send(view=view)
 
 ##### confession process
-async def set_confession(cc_channel, member, type: str):
-    overwrite = discord.PermissionOverwrite()
 
-    overwrite.view_channel = False
-    everyone = get(member.guild.roles, id=guild_id)
-    await cc_channel.set_permissions(everyone, overwrite=overwrite)
-    overwrite.view_channel = True
-    await cc_channel.set_permissions(member, overwrite=overwrite)
+class Confession():
 
-    embed = discord.Embed(
-        title="**Nơi " + member.name + " tâm sự những câu chuyện thầm kín**",
-        description=
-        "Chúng mình luôn lắng nghe và chia sẻ mọi buồn vui cùng bạn",
-        colour=discord.Colour.gold())
-    embed.set_thumbnail(url=member.avatar.url)
-    embed.add_field(
-        name="**Chú ý**",
-        value="Kênh sẽ biến mất sau 30 phút hoặc bạn gõ lệnh ``/end_confession`` ",
-        inline=False)
-    embed.set_footer(text='''BetterMe - Better everyday''')
+    def __init__(self, cc_channel: discord.TextChannel, member: Optional[Union[discord.Member, discord.User]], cfs_type: str):
+        self.cc_channel = cc_channel
+        self.member = member
+        self.cfs_type = cfs_type
 
-    db = open_database(database_directory)
-    db[str(cc_channel.id)] = {"user_id": member.id, "type": type}
-    write_database(db, database_directory)
-    await cc_channel.send(content=member.mention, embed=embed)
+        self.content = ""
+        self.files = []
 
-    await asyncio.sleep(1780)
-    # await asyncio.sleep(20)
-    if cc_channel:
-        await cc_channel.send(
-            member.mention +
-            "**Những lời tâm tư ngàn lời không nói hết. Nếu bạn muốn tiếp tục tâm sự hãy tạo 1 kênh mới. Kênh này sẽ biến mất sau 2 phút nữa. **"
-        )
-        await asyncio.sleep(120)
-        # await asyncio.sleep(10)
+    async def set_confession(self):
+        overwrite = discord.PermissionOverwrite()
 
+        overwrite.view_channel = False
+        everyone = get(self.member.guild.roles, id=guild_id)
+        await self.cc_channel.set_permissions(everyone, overwrite=overwrite)
+        overwrite.view_channel = True
+        await self.cc_channel.set_permissions(self.member, overwrite=overwrite)
 
-async def text_process(channel_id: int, mem_id: int):
-    #test
-    channel = get(bot.get_all_channels(), id=channel_id)
-    messages = [message async for message in channel.history(limit=200)]
-    for message in messages:
-        if message.author.id == mem_id:
-            db = open_database(database_directory)
-            if message.content not in ["", "m,end", "M,end"]:
-                db[str(message.author.id)]["content"] += message.content + ". "
-            if message.attachments:
-                for attach in message.attachments:
-                    image = save_image(attach.url)
-                    db = open_database(database_directory)
-                    db[str(message.author.id)]["files"].append(image)
-            write_database(db, database_directory)
+        embed = discord.Embed(
+            title="**Nơi " + self.member.name + " tâm sự những câu chuyện thầm kín**",
+            description=
+            "Chúng mình luôn lắng nghe và chia sẻ mọi buồn vui cùng bạn",
+            colour=discord.Colour.gold())
+        embed.set_thumbnail(url=self.member.avatar.url)
+        embed.add_field(
+            name="**Chú ý**",
+            value="Kênh sẽ biến mất sau 30 phút hoặc bạn gõ lệnh ``/end_confession`` ",
+            inline=False)
+        embed.set_footer(text='''BetterMe - Better everyday''')
 
-
-async def send_files(file_list: list):
-    files_to_read: list[str] = file_list
-    files_to_send: list[discord.File] = []
-    for filename in files_to_read:
-        with open(filename, 'rb') as f:
-            files_to_send.append(discord.File(f))
-        await delete_image(filename)
-    return files_to_send
-
-
-async def end_confession(channel_id: int):
-    global guild
-
-    db = open_database(database_directory)
-    if str(channel_id) in db.keys():
-        mem_id = db[str(channel_id)]["user_id"]
-        await text_process(channel_id, mem_id)
-        mem_id = str(mem_id)
         db = open_database(database_directory)
-        if len(db[mem_id]["content"]) < 50 and db[mem_id]["files"] == []:
-            del db[mem_id]
+        db[str(self.cc_channel.id)] = {"user_id": self.member.id, "type": self.cfs_type}
+        write_database(db, database_directory)
+        await self.cc_channel.send(content=self.member.mention, embed=embed)
+
+        await asyncio.sleep(1780)
+        # await asyncio.sleep(20)
+        db = open_database(database_directory)
+        if str(self.cc_channel.id) in db.keys():
+            await self.cc_channel.send(
+                self.member.mention +
+                "**Những lời tâm tư ngàn lời không nói hết. Nếu bạn muốn tiếp tục tâm sự hãy tạo 1 kênh mới. Kênh này sẽ biến mất sau 2 phút nữa. **"
+            )
+            await asyncio.sleep(120)
+            # await asyncio.sleep(10)
+
+
+    async def text_process(self):
+        channel = get(bot.get_all_channels(), id=self.cc_channel.id)
+        messages = [message async for message in channel.history(limit=200)]
+        message.reverse()
+        for message in messages:
+            if message.author.id == self.member.id:
+                if message.content not in ["", "m,end", "M,end","/end_confession"]:
+                    self.content += message.content + ". "
+                if message.attachments:
+                    for attach in message.attachments:
+                        image = save_image(attach.url)
+                        self.files.append(image)
+
+
+    async def send_files(self):
+
+        files_to_read: list[str] = self.files
+        files_to_send: list[discord.File] = []
+
+        for filename in files_to_read:
+            with open(filename, 'rb') as f:
+                files_to_send.append(discord.File(f))
+            await delete_image(filename)
+        return files_to_send
+
+
+    async def end_confession(self):
+        global guild
+        channel_id = self.cc_channel.id
+
+        db = open_database(database_directory)
+        if str(channel_id) in db.keys():
+            await self.text_process()
+
+            if len(self.content) < 50 and self.files == []: 
+                message = "Nội dung confession của bạn rất ngắn nên sẽ không được gửi đi. Lưu ý nếu gửi confession khó hiểu, không có chủ đích sẽ bị mute ít nhất 3 ngày"
+            else:
+                if self.cfs_type == "private":
+                    await self.end_private_confession()
+                elif self.cfs_type == "public":
+                    await self.end_public_confession()
+                db["confession_count"] = db["confession_count"] + 1
+                message = "Cảm ơn bạn đã chia sẻ cùng chúng mình"
+                
+            await self.cc_channel.delete()
+            db = open_database(database_directory)
             del db[str(channel_id)]
             write_database(db, database_directory)
-            cc_channel = get(bot.get_all_channels(), id=channel_id)
-            await cc_channel.delete()
-            member = bot.get_user(int(mem_id))
-            await member.send("Nội dung confession của bạn rất ngắn nên sẽ không được gửi đi. Lưu ý nếu gửi confession khó hiểu, không có chủ đích sẽ bị mute ít nhất 3 ngày.")
-
-        elif db[str(channel_id)]["type"] == "private":
-            await end_private_confession(channel_id, mem_id)
-        elif db[str(channel_id)]["type"] == "public":
-            await end_public_confession(channel_id, mem_id)
+            await self.member.send(message)
+                
 
 
-async def end_private_confession(channel_id: int, mem_id: str):
-    global guild, confession_channel, private_confession_channel
+    async def end_private_confession(self):
+        global guild, confession_channel, private_confession_channel
 
-    member = await bot.fetch_user(int(mem_id))
-    db = open_database(database_directory)
+        db = open_database(database_directory)
+        files = await self.send_files()
 
-    content = "``` Confession " + str(db["confession_count"]) + "-ẩn danh```"
-    embed = discord.Embed(title="**Tâm sự ẩn danh**",
-                          description=db[str(mem_id)]["content"],
-                          colour=discord.Colour.gold())
-    files = await send_files(db[str(mem_id)]["files"])
-    embed.set_footer(text='''BetterMe - Better everyday''')
-    await private_confession_channel.send(content=content, embed=embed, files=files)
-    embed.add_field(name="**Id**", value="||<@" + mem_id + ">||", inline=False)
-    await confession_channel.send(content=content, embed=embed, files=files)
+        content = "``` Confession " + str(db["confession_count"]) + "-ẩn danh```"
+        embed = discord.Embed(title="**Tâm sự ẩn danh**", description=self.content, colour=discord.Colour.gold())
+        embed.set_footer(text='''BetterMe - Better everyday''')
+        await private_confession_channel.send(content=content, embed=embed, files=files)
+        embed.add_field(name="**Id**", value=f"||{self.member.mention}||", inline=False)
+        await confession_channel.send(content=content, embed=embed, files=files)
 
-    db = open_database(database_directory)
-    db["confession_count"] = db["confession_count"] + 1
-    del db[mem_id]
-    del db[str(channel_id)]
-    write_database(db, database_directory)
+    async def end_public_confession(self):
+        global guild, confession_channel, private_confession_channel
 
-    cc_channel = get(bot.get_all_channels(), id=channel_id)
-    await cc_channel.delete()
-    await member.send("Cảm ơn bạn đã chia sẻ cùng chúng mình")
+        db = open_database(database_directory)
+        files = await self.send_files()
 
-
-async def end_public_confession(channel_id: int, mem_id: str):
-    global guild, confession_channel, private_confession_channel
-
-    member = await bot.fetch_user(int(mem_id))
-    db = open_database(database_directory)
-    files = await send_files(db[str(mem_id)]["files"])
-
-    content = "``` Confession " + str(db["confession_count"]) + "-công khai```"
-    embed = discord.Embed(title="**Tâm sự của " + member.name + "**",
-                          description=db[str(mem_id)]["content"],
-                          colour=discord.Colour.gold())
-    embed.add_field(name="**Id**", value="||<@" + mem_id + ">||", inline=False)
-    embed.set_thumbnail(url=member.avatar.url)
-    embed.set_footer(text='''BetterMe - Better everyday''')
-    await private_confession_channel.send(content=content,
-                                        embed=embed,
-                                        files=files)
-    await confession_channel.send(content=content, embed=embed, files=files)
-
-    db = open_database(database_directory)
-    db["confession_count"] = db["confession_count"] + 1
-    del db[mem_id]
-    del db[str(channel_id)]
-    write_database(db, database_directory)
-
-    cc_channel = get(bot.get_all_channels(), id=channel_id)
-    await cc_channel.delete()
-    await member.send("Cảm ơn bạn đã chia sẻ cùng chúng mình")
+        content = "``` Confession " + str(db["confession_count"]) + "-công khai```"
+        embed = discord.Embed(title="**Tâm sự của " + self.member.name + "**", description=self.content, colour=discord.Colour.gold())
+        embed.add_field(name="**Id**", value=f"||{self.member.mention}||", inline=False)
+        embed.set_thumbnail(url=self.member.avatar.url)
+        embed.set_footer(text='''BetterMe - Better everyday''')
+        await private_confession_channel.send(content=content,embed=embed,files=files)
+        await confession_channel.send(content=content, embed=embed, files=files)
 
 
 # thực hiện lệnh
 @bot.tree.command(name="end_confession", description="Kết thúc confession")
 async def end(interaction: Interaction):
     await interaction.response.defer()
-    await end_confession(interaction.channel.id)
+    db = open_database(database_directory)
+    if str(interaction.channel.id) in db.keys():
+        mem_id = db[str(interaction.channel.id)]["user_id"]
+        cfs_type = db[str(interaction.channel.id)]["type"]
+
+        member = bot.get_user(mem_id)
+        confession = Confession(cc_channel=interaction.channel, member=member, cfs_type=cfs_type)
+        await confession.end_confession()
 
 
 @bot.listen()
 async def on_interaction(interaction: Interaction):
-    if interaction.message.id == confession_dropdown_id and interaction.type == discord.InteractionType.component:
-        
-        db = open_database(database_directory)
-        member = interaction.user
-        values = interaction.data["values"]
-        await interaction.response.defer()
-        if str(member.id) in db.keys():
-            await member.send("Bạn chỉ có thể tạo 1 kênh confession 1 lúc")
-        elif "private-confession" in values or "public-confession" in values:
-            db[str(member.id)] = {"content": "", "files": []}
-            member = interaction.user
-            cc_name = channel_name(member.name, "confession")
-            category = bot.get_channel(confession_category_id)
-            cc_channel = await category.create_text_channel(cc_name,reason=None)
-
-            #confession
-            write_database(db, database_directory)
-            if "private-confession" in values:
-                await set_confession(cc_channel, member, "private")
-            elif "public-confession" in values:
-                await set_confession(cc_channel, member, "public")
-
+    if interaction.message:
+        if interaction.message.id == confession_dropdown_id and interaction.type == discord.InteractionType.component:
+            
             db = open_database(database_directory)
+            member = interaction.user
+            values = interaction.data["values"]
+            await interaction.response.defer()
+            
             if str(member.id) in db.keys():
-                await end_confession(cc_channel.id)
-            else:
-                print("not found " + member.name)
+                await member.send("Bạn chỉ có thể tạo 1 kênh confession 1 lúc")
+            elif "private-confession" in values or "public-confession" in values:
+                member = interaction.user
+                cc_name = channel_name(member.name, "confession")
+                category = bot.get_channel(confession_category_id)
+                cc_channel = await category.create_text_channel(cc_name,reason=None)
+
+                #confession
+                if "private-confession" in values:
+                    cfs_type = "private"
+                elif "public-confession" in values:
+                    cfs_type = "public"
+                
+                confession = Confession(cc_channel=cc_channel, member= member, cfs_type=cfs_type)
+                await confession.set_confession()
+
+                await confession.end_confession()
+
+
+async def fix_confession():
+    pass

f67e1d50dce9c788c64e569763ab5763bcf54454
Beowulf Cluster <dbsiksfikf@gmail.com>
2022-09-20 22:01:48 +0700

    fix

--- security/introduce.py
+++ security/introduce.py
@@ -24,6 +24,6 @@ async def on_message(message):
       ...
       ||**Trên 120 từ nhé :3**||
     '''
-    embed.set_thumbnail(url=message.author.avatar_url)
+    embed.set_thumbnail(url=message.author.avatar.url)
     embed.add_field(name="**Một mẫu giới thiệu cơ bản**",value=basic,inline=False)
     await message.author.send(embed = embed)

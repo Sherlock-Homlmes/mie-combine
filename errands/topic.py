c459fd88cba7d784bd5037721c497d14b31bec95
Beowulf Cluster <dbsiksfikf@gmail.com>
2022-09-20 14:45:23 +0700

    fix topic

--- errands/topic.py
+++ errands/topic.py
@@ -1,21 +1,20 @@
 from base import (
   # necess
-  bot,tasks,get,
+  bot,tasks,get,Interaction,
   # var
   color_roles
   )
 
-import json
 import random
 
 from feature_func.stable_json import open_database
 
 @bot.tree.command(name="etopic", description="Gợi ý 1 topic tiếng Anh")
-async def etopic(ctx):
+async def etopic(interaction: Interaction):
   topic = random.choice(open_database("/topic/etopic"))
-  await ctx.send(f"Chủ đề: **{topic}**")
+  await interaction.response.send_message(f"Chủ đề: **{topic}**")
 
 @bot.tree.command(name="vtopic", description="Gợi ý 1 topic tiếng Việt")
-async def vtopic(ctx):
+async def vtopic(interaction: Interaction):
   topic = random.choice(open_database("/topic/vtopic"))
-  await ctx.send(f"Chủ đề: **{topic}**")
+  await interaction.response.send_message(f"Chủ đề: **{topic}**")

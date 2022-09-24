c3cd7241b2a8ff360ac93bf16af2448dc2d29319
Beowulf Cluster <dbsiksfikf@gmail.com>
2022-09-24 18:47:13 +0700

    fix ai chatbot resource

--- feature_func/mongodb/ai_chatbot.py
+++ feature_func/mongodb/ai_chatbot.py
@@ -10,14 +10,14 @@ def create_data(value: list):
     global number_docs
 
     # number
-    number += 1
+    number_docs += 1
     # time stamp
     utc_now = pytz.utc.localize(datetime.datetime.utcnow())
     pst_now = utc_now.astimezone(pytz.timezone("Asia/Ho_Chi_Minh"))
 
     # insert data
     data = {}
-    data["id"] = number + 1
+    data["id"] = number_docs + 1
     data["time"] = pst_now
     data["value"] = value
     dtb.insert_one(data)

db39019ac5f3df765d10669cf2c2de6d81f8882f
LinuxDragon <owner@betterme.study>
2023-02-17 00:15:03 +0700

    unlock study time

--- bot/features/study_time/study_time.py
+++ bot/features/study_time/study_time.py
@@ -19,47 +19,46 @@ async def on_voice_state_update(
     member_before: discord.VoiceState,
     member_after: discord.VoiceState,
 ):
-    if member.id == 880359404036317215:
-        if not member_before.channel and member_after.channel:
-            while member.id in updating_members:
-                await asyncio.sleep(1)
-            updating_members.append(member.id)
-            print(member.name, member.id, "in")
-            try:
-                user_study_section = await UserStudySection.find_one(
-                    UserStudySection.user.discord_id == str(member.id),
-                    fetch_links=True,
-                )
-                if user_study_section:
-                    user_study_section.start_study_time = Now().now
-                    await user_study_section.save()
-                else:
-                    await UserStudySection(
-                        user=await Users.find_one(Users.discord_id == str(member.id)),
-                        start_study_time=Now().now,
-                    ).insert()
-
-            # if user not in users database
-            except ValidationError as e:
-                print(e)
-                await on_member_join(member=member)
+    if not member_before.channel and member_after.channel:
+        while member.id in updating_members:
+            await asyncio.sleep(1)
+        updating_members.append(member.id)
+        print(member.name, member.id, "in")
+        try:
+            user_study_section = await UserStudySection.find_one(
+                UserStudySection.user.discord_id == str(member.id),
+                fetch_links=True,
+            )
+            if user_study_section:
+                user_study_section.start_study_time = Now().now
+                await user_study_section.save()
+            else:
                 await UserStudySection(
                     user=await Users.find_one(Users.discord_id == str(member.id)),
                     start_study_time=Now().now,
                 ).insert()
 
-        elif member_before.channel and not member_after.channel:
-            print(member.name, member.id, "out")
-            user_study_section = await UserStudySection.find_one(
-                UserStudySection.user.discord_id == str(member.id), fetch_links=True
-            )
-            try:
-                await user_study_section.delete()
-            except AttributeError as e:
-                print(e)
-            if member.id in updating_members:
-                updating_members.remove(member.id)
-                print("not in updating member anymore")
+        # if user not in users database
+        except ValidationError as e:
+            print(e)
+            await on_member_join(member=member)
+            await UserStudySection(
+                user=await Users.find_one(Users.discord_id == str(member.id)),
+                start_study_time=Now().now,
+            ).insert()
+
+    elif member_before.channel and not member_after.channel:
+        print(member.name, member.id, "out")
+        user_study_section = await UserStudySection.find_one(
+            UserStudySection.user.discord_id == str(member.id), fetch_links=True
+        )
+        try:
+            await user_study_section.delete()
+        except AttributeError as e:
+            print(e)
+        if member.id in updating_members:
+            updating_members.remove(member.id)
+            print("not in updating member anymore")
 
 
 @bot.tree.command(name="study_time", description="Xem tổng thời gian học")

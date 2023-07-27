7e2bfea2803141b1f0aa69c0f6359634511db252
Ta Minh Khoi <khoitm@codenet.jp>
2023-07-27 23:48:02 +0700

    fix: cannot see confession

--- other_modules/discord_bot/overwrite.py
+++ other_modules/discord_bot/overwrite.py
@@ -5,9 +5,6 @@ import asyncio
 # lib
 import discord
 
-# local
-from bot import server_info
-
 
 class Overwrite(discord.PermissionOverwrite):
     async def create_confession(
@@ -15,14 +12,6 @@ class Overwrite(discord.PermissionOverwrite):
         member: Union[discord.Member, discord.User],
         channel: Union[discord.VoiceChannel, discord.TextChannel],
     ):
-        global every_one_role
-
-        # Set permission for everyone not see channel
-        self.view_channel = False
-        await channel.set_permissions(
-            server_info.every_one_role,
-            overwrite=self)
-
         # Set permission for member to see channel
         self.view_channel = True
         await channel.set_permissions(member, overwrite=self)

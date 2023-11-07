8639360a169172b2da351d4d046615890db2677d
LinuxDragon <linuxdragon@pop-os.localdomain>
2023-11-07 14:52:23 +0700

    feat: introduce add thread at hello

--- bot/features/security/introduce.py
+++ bot/features/security/introduce.py
@@ -8,29 +8,33 @@ from bot import bot
 
 @bot.listen()
 async def on_message(message: discord.Message):
-    if (
-        str(message.channel) == "giới-thiệu-bản-thân"
-        and message.author.bot is False
-        and len(message.content) < 120
-    ):
-        await message.delete()
-        embed = discord.Embed(
-            title="**Chào mừng "
-            + message.author.name
-            + " đến với Cộng Đồng học tập BetterMe**",
-            description="Bạn giới thiệu đầy đủ 1 xíu nha.",
-            colour=discord.Colour.gold(),
-        )
-        basic = """
-      Tên tuổi
-      Năm sinh
-      Nơi ở
-      Sở thích cá nhân
-      Điểm mạnh, yếu
-      Mục tiêu trong tương lai
-      ...
-      ||**Trên 120 từ nhé :3**||
-    """
-        embed.set_thumbnail(url=message.author.avatar.url)
-        embed.add_field(name="**Một mẫu giới thiệu cơ bản**", value=basic, inline=False)
-        await message.author.send(embed=embed)
+    if str(message.channel) == "giới-thiệu-bản-thân" and message.author.bot is False:
+        if len(message.content) < 120:
+            await message.delete()
+            embed = discord.Embed(
+                title="**Chào mừng "
+                + message.author.name
+                + " đến với Cộng Đồng học tập BetterMe**",
+                description="Bạn giới thiệu đầy đủ 1 xíu nha.",
+                colour=discord.Colour.gold(),
+            )
+            basic = """
+        Tên tuổi
+        Năm sinh
+        Nơi ở
+        Sở thích cá nhân
+        Điểm mạnh, yếu
+        Mục tiêu trong tương lai
+        ...
+        ||**Trên 120 từ nhé :3**||
+        """
+            embed.set_thumbnail(url=message.author.avatar.url)
+            embed.add_field(
+                name="**Một mẫu giới thiệu cơ bản**", value=basic, inline=False
+            )
+            await message.author.send(embed=embed)
+
+        else:
+            await message.create_thread(
+                name="Mọi người vào chào bạn mới đi nào!", auto_archive_duration=10080
+            )

09836dabb5a11765ccb2297e5d5b81a5841130e0
LinuxDragon <linuxdragon@pop-os.localdomain>
2023-11-07 15:39:06 +0700

    fix: remove all env

--- all_env.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-my_secret: str = os.environ.get("bot_token")
-database_url: str = os.environ.get("database_url")
-bot_only: bool = os.environ.get("bot_only", default=False)

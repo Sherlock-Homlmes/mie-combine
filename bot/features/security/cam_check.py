7b584766bf2a3d7c026f40795a9e20d626af9e96
LinuxDragon <linuxdragon@pop-os.localdomain>
2023-12-15 02:26:27 +0700

    refactor: better cam check code

--- bot/features/security/cam_check.py
+++ bot/features/security/cam_check.py
@@ -15,7 +15,7 @@ from bot import bot, server_info
 class CheckCamEmbedMessage:
     # value input
     member: discord.Member = None
-    check_type: List = None
+    check_types: List = None
 
     # value after create
     title: str = None
@@ -28,9 +28,7 @@ class CheckCamEmbedMessage:
     async def send(self):
         self.update_embed()
         try:
-            self.message = await self.member.send(
-                content=self.member.mention, embed=self.embed
-            )
+            self.message = await self.member.send(content=self.member.mention, embed=self.embed)
         except Exception as e:
             print("Cam check Error:", e)
 
@@ -57,26 +55,20 @@ class CheckCamEmbedMessage:
     def warn(self):
         self.title = "Nhắc nhở"
         self.coulour = discord.Colour.orange()
-        if self.check_type == ["cam"]:
-            self.description = "bạn đang ở trong phòng FULL CAM. Hãy bật camera, nếu không bạn sẽ bị kick sau 1 phút"
-        elif self.check_type == ["cam", "stream"]:
-            self.description = "bạn đang ở trong phòng CAM/STREAM. Hãy bật camera/stream, nếu không bạn sẽ bị kick sau 1 phút"
+        room_type = "/".join(self.check_types)
+        self.description = f"bạn đang ở trong phòng FULL CAM. Hãy bật {room_type}, nếu không bạn sẽ bị kick sau 1 phút"
 
     def punish(self):
         self.title = "Nhắc nhở"
         self.coulour = discord.Colour.red()
-        if self.check_type == ["cam"]:
-            self.description = "bạn đã bị kick ra khỏi phòng vì không bật cam"
-        elif self.check_type == ["cam", "stream"]:
-            self.description = "bạn đã bị kick ra khỏi phòng vì không bật cam/tream"
+        room_type = "/".join(self.check_types)
+        self.description = f"bạn đã bị kick ra khỏi phòng vì không bật {room_type}"
 
     def thanks_for_accept(self):
         self.title = "Cảm ơn"
         self.coulour = discord.Colour.green()
-        if self.check_type == ["cam"]:
-            self.description = "cảm ơn bạn đã bật cam"
-        elif self.check_type == ["cam", "stream"]:
-            self.description = "cảm ơn bạn đã bật cam/stream"
+        room_type = "/".join(self.check_types)
+        self.description = f"cảm ơn bạn đã bật {room_type}"
 
     def thanks_for_leave(self):
         self.title = "Cảm ơn"
@@ -98,21 +90,26 @@ async def on_voice_state_update(
     full_cam_channels = server_info.full_cam_channels
     cam_stream_channels = server_info.cam_stream_channels
     sleep_time = [30, 50]
+    check_channels = None
 
-    ### only cam
-    if (
-        member_after.channel in full_cam_channels
-        and member.id not in check_cam_member_ids
-    ):
+    if member_after.channel in full_cam_channels and member.id not in check_cam_member_ids:
+        check_channels = full_cam_channels
+        check_types = ["cam"]
+    elif member_after.channel in cam_stream_channels and member.id not in check_cam_member_ids:
+        check_channels = cam_stream_channels
+        check_types = ["cam", "stream"]
+
+    if check_channels:
         check_cam_member_ids.append(member.id)
         await asyncio.sleep(sleep_time[0])
         # remind
         if member.voice is not None:
-            if (
-                member.voice.self_video is False
-                and member.voice.channel in full_cam_channels
-            ):
-                embed = CheckCamEmbedMessage(member=member, check_type=["cam"])
+            check_type_map = {
+                "cam": member.voice.self_video,
+                "stream": member.voice.self_stream,
+            }
+            if not any(check_type_map[check_type] for check_type in check_types) and member.voice.channel in check_channels:
+                embed = CheckCamEmbedMessage(member=member, check_types=check_types)
                 embed.warn()
                 await embed.send()
 
@@ -120,44 +117,8 @@ async def on_voice_state_update(
                 await asyncio.sleep(sleep_time[1])
                 if member.voice is not None:
                     if (
-                        member.voice.self_video is False
-                        and member.voice.channel in full_cam_channels
-                    ):
-                        await member.move_to(None)
-                        embed.punish()
-                    else:
-                        embed.thanks_for_accept()
-                else:
-                    embed.thanks_for_leave()
-
-                await embed.update()
-        check_cam_member_ids.remove(member.id)
-
-    ### cam | stream
-    if (
-        member_after.channel in cam_stream_channels
-        and member.id not in check_cam_member_ids
-    ):
-        check_cam_member_ids.append(member.id)
-        await asyncio.sleep(sleep_time[0])
-        # remind
-        if member.voice is not None:
-            if (
-                not any([member.voice.self_video, member.voice.self_stream])
-                and member.voice.channel in cam_stream_channels
-            ):
-                embed = CheckCamEmbedMessage(
-                    member=member, check_type=["cam", "stream"]
-                )
-                embed.warn()
-                await embed.send()
-
-                # kick
-                await asyncio.sleep(sleep_time[1])
-                if member.voice is not None:
-                    if (
-                        not any([member.voice.self_video, member.voice.self_stream])
-                        and member.voice.channel in cam_stream_channels
+                        not any(check_type_map[check_type] for check_type in check_types)
+                        and member.voice.channel in check_channels
                     ):
                         await member.move_to(None)
                         embed.punish()

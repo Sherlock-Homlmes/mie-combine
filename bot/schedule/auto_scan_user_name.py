4d8829b586b51b7c7651d0df50295313c9990cbd
gaga <theengineer9908@gmail.com>
2025-01-16 22:45:24 +0700

    feat: auto scan user name

---
+++ bot/schedule/auto_scan_user_name.py
@@ -0,0 +1,21 @@
+# default
+import asyncio
+
+# lib
+from discord.ext import tasks
+
+# local
+from core.conf.bot.conf import server_info
+from bot.confession import check_bad_words
+
+
+@tasks.loop(hours=24)
+async def auto_scan_user_name():
+    warning_members = []
+    async for member in server_info.guild.fetch_members():
+        if not check_bad_words(member.name or "") or not check_bad_words(member.nick or ""):
+            warning_members.append(member)
+
+    for member in warning_members:
+        await member.send("Tên của bạn có từ cấm. Hãy sửa lại tên phù hợp với quy định của server")
+        await asyncio.sleep(1)

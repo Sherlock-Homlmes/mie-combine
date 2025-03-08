51882af0366b7d4954afd1bd35beeb6e0bc9d58a
gaga <theengineer9908@gmail.com>
2025-03-08 11:45:29 +0700

    fix: exception when send message to bad user

--- bot/schedule/auto_scan_user_name.py
+++ bot/schedule/auto_scan_user_name.py
@@ -2,6 +2,7 @@
 import asyncio
 
 # lib
+import discord
 from discord.ext import tasks
 
 from bot.confession import check_bad_words
@@ -19,5 +20,10 @@ async def scan_user_name():
             warning_members.append(member)
 
     for member in warning_members:
-        await member.send("Tên của bạn có từ cấm. Hãy sửa lại tên phù hợp với quy định của server")
+        try:
+            await member.send(
+                "Tên của bạn có từ cấm. Hãy sửa lại tên phù hợp với quy định của server"
+            )
+        except discord.errors.Forbidden:
+            pass
         await asyncio.sleep(1)

7e7f52f4ef720f7f4d7fcf93578b03ac01a6e529
Ta Minh Khoi <khoitm@otani-trading.com>
2025-10-07 19:12:45 +0700

    fix: enable all feature

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,13 +1,13 @@
 # Event and schedule
-# from .ai import *
-# from .confession import *
-# from .create_vc import *
-# from .errands import *
-# from .guild_event import *
+from .ai import *
+from .confession import *
+from .create_vc import *
+from .errands import *
+from .guild_event import *
 from .money import *
-# from .schedule import *
-# from .security import *
-# from .study_time import *
+from .schedule import *
+from .security import *
+from .study_time import *
 
 
 ###### not implement

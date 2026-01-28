2b279c6fb9b1898283a3cd9ba7d31ff86d5a8d18
Sherlock-Homlmes <dbsiksfikf@gmail.com>
2026-01-29 01:32:58 +0700

    Fix: do not auto start bot

--- bot/schedule/__init__.py
+++ bot/schedule/__init__.py
@@ -4,7 +4,7 @@ import asyncio
 # local
 from core.conf.bot.conf import bot
 
-from .auto_restart_bot import restart_bot
+# from .auto_restart_bot import restart_bot
 from .auto_scan_user_name import scan_user_name
 from .static_channels import static_channels
 from .monthly_leaderboard import auto_reset_role_monthly, leaderboard_monthly
@@ -13,7 +13,7 @@ from .monthly_leaderboard import auto_reset_role_monthly, leaderboard_monthly
 @bot.listen()
 async def on_ready():
     await bot._fully_ready.wait()
-    restart_bot.start()
+    # restart_bot.start()
     scan_user_name.start()
     static_channels.start()
     auto_reset_role_monthly.start()
